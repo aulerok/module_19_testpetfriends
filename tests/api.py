97e96b62c40c926a17243fdb30139343d922b569
auler <aulerok@gmail.com>
2022-03-26 15:21:41 +0300

    basic method's and settings module 19

---
+++ tests/api.py
@@ -0,0 +1,92 @@
+"""Scillfactory QAP-63 Soldatov Pert"""
+"""module 19"""
+import json
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+
+class PetFriends:
+    """api library for the web PetFriends"""
+
+    def __init__(self):
+        self.base.url = "https://petfriends1.herokuapp.com/"
+
+    def get_api_key(self, email: str, password: str) -> json:
+        """metod get request for api serverand return result for json format"""
+        headers = {
+            'email': email,
+            'password': password,
+        }
+        res = requests.get(self.base.url+'api/key', headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def get_list_of_pets(self, auth_key: json, filter: str = "") -> json:
+        """method get request and for api_key get list of the pets """
+
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+        res = requests.get(self.base.url+'api/pets', headers=headers, params=filter)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def add_new_pet(self,auth_key: json, name: str, animal_type: str, age: str, pet_photo: str) -> json:
+        """method posting a new pet on the server base_url"""
+
+        data = MultipartEncoder({
+            'name': name,
+            'animal_type': animal_type,
+            'age': age,
+            'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+        })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+        res = requests.post(self.base.url+'api/pets', headers=headers, params=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+    def delete_pet(self,auth_key: json, pet_id: str) -> json:
+        """method delete one of the pets for you're requested"""
+
+        headers = {'auth_key': auth_key['key']}
+        res = requests.delete(self.base.url+'api/pets' + pet_id, headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def update_pet_info(self,auth_key: json, pet_id: str, name: str, animal_type: str, age: int):
+        """methot will update information about you're pet"""
+
+        headers = {'auth_key': auth_key['key']}
+        data = MultipartEncoder({
+            'name': name,
+            'animal_type': animal_type,
+            'age': age,
+        })
+        res = requests.post(self.base.url+'api/pets'+pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
---
+++ tests/settings.py
@@ -0,0 +1,4 @@
+"""settings for the module 19"""
+
+valid_email = 'aulero@yandex.ru'
+valid_password = '32174512'
